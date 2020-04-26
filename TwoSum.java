ebfd0465f61472ca37e2cb408c6457ff265953eb
Sai Sree Harish Nune <nune.harish@gmail.com>
2020-04-26 18:49:56 +0530

    Returning the indices of Two numbers to get Target.

---
+++ TwoSum.java
@@ -0,0 +1,23 @@
+package java8;
+
+public class TwoSum {
+	
+	 public static int[] twoSum(int[] nums, int target) {
+	        
+	        for(int i=0;i<nums.length-1;i++){
+	            for(int j=1;j<nums.length;j++){
+	                if(nums[i]+nums[j] == target && i!=j){
+	                    return new int[]{i,j};
+	                }
+	            }
+	        }
+	        return new int[]{};
+	        
+	       
+	    }
+
+	public static void main(String[] args) {
+		System.out.println(twoSum(new int[] {2, 7, 11, 15},6));
+	}
+
+}
